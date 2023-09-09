538b398f0a5541511222d955ae97a6f06d9ed3c4
yusei53 <yusei530922@gmail.com>
2023-09-10 01:15:07 +0900

    headerの細かい修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
 import { Box } from "@chakra-ui/react";
-import { css } from "@emotion/react";
 
 export default function Header() {
   return (
     <>
-      <Box display={"flex"} backgroundColor={"#ff9900"}>
+      <Box display={"flex"} backgroundColor={"#ff9900"} as="header">
         <Box color={"white"} fontSize={"40px"} fontWeight={700} margin={1}>
           イーズミー
         </Box>
