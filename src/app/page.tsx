7c6c8a7102d2094f9dba8d1aef1a0e7c7a97620f
yusei53 <yusei530922@gmail.com>
2023-09-09 18:14:37 +0900

    emotion動作確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,18 @@
 "use client";
 
-import { Button, Flex, Heading, Input } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { css } from "@emotion/react";
 
 export default function Home() {
   return (
-    <Flex height="100vh" alignItems="center" justifyContent="center">
-      <Flex direction="column" background="gray.100" padding={12} rounded={6}>
-        + <Heading mb={6}>Log in</Heading>+{" "}
-        <Input
-          placeholder="sample@sample.com"
-          variant="filled"
-          mb={3}
-          type="email"
-        />
-        +{" "}
-        <Input placeholder="********" variant="filled" mb={6} type="password" />
-        +{" "}
-        <Button mb={6} colorScheme="teal">
-          Log in
-        </Button>
-        + <Button>Toggle Color Mode</Button>
-      </Flex>
-    </Flex>
+    <Box
+      css={css`
+        color: #fcaf17;
+        font-size: 55px;
+        font-weight: 900;
+      `}
+    >
+      イーズミー
+    </Box>
   );
 }
