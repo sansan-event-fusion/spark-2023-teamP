d249ffcfe2d2c6dcab798b56e953230064f5a640
yusei53 <yusei530922@gmail.com>
2023-09-10 20:04:25 +0900

    Headerの色変更

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,11 @@ import { Box } from "./common";
 export default function Header() {
   return (
     <>
-      <Box display={"flex"} backgroundColor={"#ff9900"} as="header">
+      <Box
+        display={"flex"}
+        background={"linear-gradient(0deg, #ff9900, #ff6600)"}
+        as="header"
+      >
         <Box color={"white"} fontSize={"40px"} fontWeight={700} margin={1}>
           イーズミー
         </Box>
