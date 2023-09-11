819f841ce13d12f57d9a096291bf0ff4d5a4b416
yusei53 <yusei530922@gmail.com>
2023-09-11 14:17:54 +0900

    募集ボタン設置

--- src/app/components/Recruitment.tsx
+++ src/app/components/Recruitment.tsx
@@ -1,9 +1,19 @@
 // import RecruitmentList from "./RecruitmentList";
 
+import RecruitmentButton from "./RecruitmentButton";
+import RecruitmentCard from "./RecruitmentCard";
+
 // イメージを書いておきます
 
 export default function Recruitment() {
   //   const recruitments = await getRecruitments();
 
-  return <div>{/* <RecruitmentList recruitments={recruitments}/> */}</div>;
+  return (
+    <div>
+      <RecruitmentCard />
+      <RecruitmentButton />
+      {/* <RecruitmentList recruitments={recruitments}/> */}
+    </div>
+    /* 本番はRecruitmentCardじゃなくてRecruitmentListを書く */
+  );
 }
