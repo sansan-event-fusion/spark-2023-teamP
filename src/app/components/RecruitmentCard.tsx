90cb380165dce611f01febdd6388c8206b4a69bd
yusei53 <yusei530922@gmail.com>
2023-09-13 16:50:21 +0900

    taget.tsを使う

--- src/app/components/RecruitmentCard.tsx
+++ src/app/components/RecruitmentCard.tsx
@@ -1,6 +1,8 @@
 import { TRecruitment } from "../type";
-import { Box, Image } from "./common";
+import Capsule from "./Capsule";
+import { Box, Image, Text } from "./common";
 import NextLink from "next/link";
+import { FindColorScheme } from "../target";
 
 export default function RecruitmentCard({
   recruitment,
@@ -20,6 +22,18 @@ export default function RecruitmentCard({
           <Image width={400} src={recruitment.imgUrl} alt={recruitment.title} />
           <Box m="3" fontWeight="bold" fontSize={20} as="h1">
             {recruitment.title}
+          </Box>
+          <Box ms={3}>
+            {recruitment.targets.map((target) => (
+              <Capsule
+                key={target}
+                bg={FindColorScheme(target)}
+                fontSize="0.8em"
+                marginRight="0.5em"
+              >
+                <Text>{target}</Text>
+              </Capsule>
+            ))}
           </Box>
           <Box
             display="flex"
