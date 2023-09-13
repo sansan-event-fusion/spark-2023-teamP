0a5d5c0f4c2c4b96c4761b265d47110a99af37cd
yusei53 <yusei530922@gmail.com>
2023-09-13 17:16:55 +0900

    helper.tsにtypeの変更を反映

--- src/app/api/helper.ts
+++ src/app/api/helper.ts
@@ -2,75 +2,75 @@ import { TArticle, TRecruitment, TRoomMessage } from "../type";
 import { recruitmentList, recruitmentDetail, roomDetail } from "./client";
 
 export async function getRecruitments(): Promise<TRecruitment[]> {
-    const params = undefined;
-    const body = undefined;
-    const data = await recruitmentList(params, body);
+  const params = undefined;
+  const body = undefined;
+  const data = await recruitmentList(params, body);
 
-    return data.map(({
-        id,
-        image,
-        title,
-        organizer: {
-            name,
-            profileImage,
-        },
-        created_at,
-        peopleLimit,
-        participantsCount,
+  return data.map(
+    ({
+      id,
+      image,
+      title,
+      organizer: { name, profileImage },
+      created_at,
+      peopleLimit,
+      participantsCount,
     }) => ({
-        id,
-        name,
-        user_id: 0,
-        imgUrl: "",
-        title,
-        peopleLimit,
-        createdAt: created_at.toString(),
-        updatedAt: ""
-    }));
+      id,
+      name,
+      user_id: 0,
+      imgUrl: "",
+      title,
+      targets: [],
+      peopleLimit,
+      createdAt: created_at.toString(),
+      updatedAt: "",
+    })
+  );
 }
 
 export async function getRoomChat(roomId: number): Promise<TRoomMessage[]> {
-    const params = { roomId };
-    const body = undefined;
-    const { users, messages } = await roomDetail(params, body);
+  const params = { roomId };
+  const body = undefined;
+  const { users, messages } = await roomDetail(params, body);
 
-    return messages.map(({ body, user_id, created_at }) => {
-        const user = users.find((user) => user.id === user_id);
+  return messages.map(({ body, user_id, created_at }) => {
+    const user = users.find((user) => user.id === user_id);
 
-        if (!user) {
-            throw new Error("user not found");
-        }
+    if (!user) {
+      throw new Error("user not found");
+    }
 
-        return {
-            user: {
-                id: user.id,
-                name: user.name,
-                profileImage: user.profileImage
-            },
-            body,
-            created_at 
-        };
-    });
+    return {
+      user: {
+        id: user.id,
+        name: user.name,
+        profileImage: user.profileImage,
+      },
+      body,
+      created_at,
+    };
+  });
 }
 
 export async function getRecruitmentDetail(id: number): Promise<TArticle> {
-    const params = { recruitmentId: id };
-    const body = undefined;
-    const data = await recruitmentDetail(params, body);
+  const params = { recruitmentId: id };
+  const body = undefined;
+  const data = await recruitmentDetail(params, body);
 
-    return {
-        user: {
-            name: data.organizer.name,
-            profileImageUrl: data.organizer.image
-        },
-        recruitment: {
-            imageUrl: "",
-            title: data.recruitment.title,
-            peopleLimit: data.recruitment.peopleLimit,
-            participantsCount: data.recruitment.participantsCount, 
-            description: data.recruitment.description,
-            targets: data.recruitment.targets,
-            area: data.recruitment.area
-        }
-    };
+  return {
+    user: {
+      name: data.organizer.name,
+      profileImageUrl: data.organizer.image,
+    },
+    recruitment: {
+      imageUrl: "",
+      title: data.recruitment.title,
+      peopleLimit: data.recruitment.peopleLimit,
+      participantsCount: data.recruitment.participantsCount,
+      description: data.recruitment.description,
+      targets: data.recruitment.targets,
+      area: data.recruitment.area,
+    },
+  };
 }
