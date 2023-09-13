3af8989cff8e80ccf25f20fff3a1faad46f31af0
Tatsuya Tanaka <kanatatsu64@gmail.com>
2023-09-13 16:04:55 +0900

    設定変更

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -9,8 +9,8 @@ import AuthProvider from "./auth/AuthProvider";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <RecoilRoot>
-      <APIClientProvider mocked={false}>
-        <AuthProvider disabled={false}>
+      <APIClientProvider mocked={true}>
+        <AuthProvider disabled={true}>
           <CacheProvider>
             <ChakraProvider>{children}</ChakraProvider>
           </CacheProvider>
