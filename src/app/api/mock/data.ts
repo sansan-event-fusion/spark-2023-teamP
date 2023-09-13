169a0452d3cd0dd2c650ae806daebb8a0b480179
yusei53 <yusei530922@gmail.com>
2023-09-13 11:42:37 +0900

    エラー修正

--- src/app/api/mock/data.ts
+++ src/app/api/mock/data.ts
@@ -1,4 +1,4 @@
-const Data = [
+export const Data = [
   {
     key: ["getRecruitments"],
     response: [
@@ -59,7 +59,6 @@ const Data = [
         area: "さいたま県",
       },
     },
-
   },
   {
     key: ["getRecruitmentDetail", 2],
@@ -86,65 +85,64 @@ const Data = [
       },
     },
   },
+  {
+    key: ["getRoomChat", 1],
+    response: [
+      {
+        user: {
+          id: 1,
+          name: "みちかず",
+          profileImage: "https://placehold.jp/250x150.png",
+        },
+        body: "応募いただきありがとうございます！！埼玉大学2年生のみちかずです。",
+        created_at: new Date("2023-9-10 11:00:00"),
+      },
+      {
+        user: {
+          id: 1,
+          name: "みちかず",
+          profileImage: "https://placehold.jp/250x150.png",
+        },
+        body: "今回のハッカソンで担当したい分野はどこですか？",
+        created_at: new Date("2023-9-10 11:01:00"),
+      },
+      {
+        user: {
+          id: 2,
+          name: "たかひろ",
+          profileImage: "https://placehold.jp/250x150.png",
+        },
+        body: "デザインとかやってみたいです。",
+        created_at: new Date("2023-9-10 12:00:00"),
+      },
+    ],
+  },
+  {
+    key: ["getRecruitmentDetail", 1],
+    response: {
+      user: {
+        name: "みちかず",
+        profileImageUrl: "https://placehold.jp/250x150.png",
+      },
+      recruitment: {
+        imageUrl: "https://placehold.jp/250x150.png",
+        title: "さいたまっこハッカソン一緒にでましょう",
+        peopleLimit: 4,
+        participantsCount: 3,
+        description:
+          "さいたま市で5/13に開催される「さいたまっこハッカソン」に一緒に参加してくれる人を募集します。私は主にRuby on Railsの経験があり、バックエンドの実装は任せてください！！",
+        targets: [
+          {
+            title: "初心者歓迎",
+          },
+          {
+            title: "楽しく",
+          },
+        ],
+        area: "さいたま県",
+      },
+    },
+  },
 ];
 
-
-    {
-        key: ["getRoomChat", 1],
-        response: [
-            {
-                user: {
-                    id: 1,
-                    name: "みちかず",
-                    profileImage: "https://placehold.jp/250x150.png"
-                },
-                body: "応募いただきありがとうございます！！埼玉大学2年生のみちかずです。",
-                created_at: new Date("2023-9-10 11:00:00"),
-            },
-            {
-                user: {
-                    id: 1,
-                    name: "みちかず",
-                    profileImage: "https://placehold.jp/250x150.png"
-                },
-                body: "今回のハッカソンで担当したい分野はどこですか？",
-                created_at: new Date("2023-9-10 11:01:00"),
-            },
-            {
-                user: {
-                    id: 2,
-                    name: "たかひろ",
-                    profileImage: "https://placehold.jp/250x150.png"
-                },
-                body: "デザインとかやってみたいです。",
-                created_at: new Date("2023-9-10 12:00:00"),
-            },
-        ]
-    },
-    {
-        key: ["getRecruitmentDetail", 1],
-        response: {
-            user: {
-                name: "みちかず",
-                profileImageUrl: "https://placehold.jp/250x150.png",
-            },
-            recruitment: {
-                imageUrl: "https://placehold.jp/250x150.png",
-                title: "さいたまっこハッカソン一緒にでましょう",
-                peopleLimit: 4,
-                participantsCount: 3, 
-                description: "さいたま市で5/13に開催される「さいたまっこハッカソン」に一緒に参加してくれる人を募集します。私は主にRuby on Railsの経験があり、バックエンドの実装は任せてください！！",
-                targets: [
-                    {
-                        title: "初心者歓迎"
-                    },
-                    {
-                        title: "楽しく"
-                    }
-                ],
-                area: "さいたま県"
-            }
-        }
-    },
-];
-
+export default Data;
